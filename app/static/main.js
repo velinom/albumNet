41bb323ab1797e614a6f334c0fb6654c055b29fa
Mark <velino.m@husky.neu.edu>
2018-02-10 12:30:29 -0500

    initial commit for website skeleton

---
+++ app/static/main.js
@@ -0,0 +1,13 @@
+function httpGetAsync(url, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", url, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
+function sendAlbumRequest() {
+    httpGetAsync("/generate", function (response){ console.log(response) });
+}
