5ef44a71388263be4cd50ad5c14b020e84c68066
bbonenfant <bonenfan5ben@gmail.com>
2018-02-10 15:42:54 -0500

    Fixed my dumb file name error

--- gan/ColorWheel.py
+++ gan/ColorWheel.py
@@ -1,35 +1,31 @@
 from PIL import Image, ImageDraw
 import math
 
-img_size = 2000
-img_half = img_size // 2
-outer_radius = 1000
-
 def getcolor(x, y):
     '''
     :param x: x-coordinate from center of circle
     :param y: y-coordinate from center of circle
-    :return: tuple (R, G, B) calculated through a 3/2 polynomial fit
+    :return: tuple (R, G, B) calculated through a poly polynomial fit
     '''
-    return getRGB((x, y), '3/2')
+    return getRGB((x, y), 'poly')
 
 def getRGB(vector, fit):
     '''
-    :param vector: (x, y) relative to center of circle
-    :param fit: Determines how vector maps to RGB. Can be 'lin', 'log', '3/2'
+    :param vector: normalized (x, y) relative to center of circle
+    :param fit: Determines how vector maps to RGB. Can be 'lin', 'log', 'poly'
     :return: tuple (R, G, B) calculated through specified fit
     '''
-    assert fit in ('lin', 'log', '3/2')
+    assert fit in ('lin', 'log', 'poly')
     color = []
     for c in ('r', 'g', 'b'):
         rotv = rotate(vector, c)
-        if rotv[0] > - (outer_radius * 0.95):
+        if rotv[0] > - 0.95:
             if fit == 'lin':
-                color.append(int(128 * (1 + (rotv[0] / outer_radius))))
+                color.append(int(128 * (1 + rotv[0])))
             elif fit == 'log':
-                color.append(int(256 * math.log2(((1 + (rotv[0] / outer_radius)) / 2) + 1)))
-            elif fit == '3/2':
-                color.append(int(90 * (1 + (rotv[0] / outer_radius)) ** (3 / 2)))
+                color.append(int(256 * math.log2(((1 + rotv[0]) / 2) + 1)))
+            elif fit == 'poly':
+                color.append(int(90 * (1 + rotv[0]) ** (3 / 2)))
         else:
             color.append(0)
     return tuple(color)
@@ -51,25 +47,32 @@ def rotate(vector, color):
     else:
         raise ValueError('Invalid Color.')
 
+def normalize(vector):
+    mag = magnitude(vector)
+    return (vector[0] / mag, vector[1] / mag)
+
 def magnitude(vector):
     if vector == (0, 0):
         return 1
     return math.sqrt(vector[0] ** 2 + vector[1] ** 2)
 
-def generateColorWheel(fit='3/2'):
+def generateColorWheel(fit='poly', size=2000):
     '''
-    :param fit: Determine how each pixel maps to RGB. Can be 'lin', 'log', '3/2'
+    :param fit: Determine how each pixel maps to RGB. Can be 'lin', 'log', 'poly'
+    :param size: Pixel length of the square box containing the wheel
     :return: Saves a png of a color wheel in the same directory.
     '''
-    im = Image.new('RGB', (img_size, img_size))
-    for x in range(img_size):
-        for y in range(img_size):
+    img_half = outer_radius = size // 2
+
+    im = Image.new('RGB', (size, size))
+    for x in range(size):
+        for y in range(size):
             pix = (x, y)
             v = (x - img_half, y - img_half)
             if magnitude(v) > outer_radius:
                 continue
 
-            im.putpixel(pix, getRGB(v, fit))
+            im.putpixel(pix, getRGB(normalize(v), fit))
 
     bigsize = (im.size[0] * 3, im.size[1] * 3)
     mask = Image.new('L', bigsize, 0)
