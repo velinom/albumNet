8cfdfe326126adc93649dbe7313325c5e2307086
robcarney <robert.edward.carney@gmail.com>
2018-02-10 04:17:44 -0500

    first pass at gpu

--- gan/album_gen.py
+++ gan/album_gen.py
@@ -168,96 +168,97 @@ def discriminator(input, is_train, reuse=False):
 def train():
     random_dim = 100
 
-    with tf.variable_scope('input'):
-        # real and fake image placholders
-        real_image = tf.placeholder(tf.float32, shape=[None, HEIGHT, WIDTH, CHANNEL], name='real_image')
-        random_input = tf.placeholder(tf.float32, shape=[None, random_dim], name='rand_input')
-        is_train = tf.placeholder(tf.bool, name='is_train')
+    with tf.device("/gpu:0"):
+        with tf.variable_scope('input'):
+            # real and fake image placholders
+            real_image = tf.placeholder(tf.float32, shape=[None, HEIGHT, WIDTH, CHANNEL], name='real_image')
+            random_input = tf.placeholder(tf.float32, shape=[None, random_dim], name='rand_input')
+            is_train = tf.placeholder(tf.bool, name='is_train')
 
-    # wgan
-    fake_image = generator(random_input, random_dim, is_train)
+        # wgan
+        fake_image = generator(random_input, random_dim, is_train)
 
-    real_result = discriminator(real_image, is_train)
-    fake_result = discriminator(fake_image, is_train, reuse=True)
+        real_result = discriminator(real_image, is_train)
+        fake_result = discriminator(fake_image, is_train, reuse=True)
 
-    d_loss = tf.reduce_mean(fake_result) - tf.reduce_mean(real_result)  # This optimizes the discriminator.
-    g_loss = -tf.reduce_mean(fake_result)  # This optimizes the generator.
+        d_loss = tf.reduce_mean(fake_result) - tf.reduce_mean(real_result)  # This optimizes the discriminator.
+        g_loss = -tf.reduce_mean(fake_result)  # This optimizes the generator.
 
-    t_vars = tf.trainable_variables()
-    d_vars = [var for var in t_vars if 'dis' in var.name]
-    g_vars = [var for var in t_vars if 'gen' in var.name]
-    # test
-    # print(d_vars)
-    global_step_g = tf.Variable(0, trainable=False)
-    learning_rate_g = 0.07
-    learning_rate_g = tf.train.exponential_decay(learning_rate_g, global_step_g, 10000, .3, staircase=True)
-    global_step_d = tf.Variable(0, trainable=False)
-    learning_rate_d = tf.train.exponential_decay(learning_rate_g, global_step_g, 10000, .3, staircase=True)
-    trainer_d = tf.train.RMSPropOptimizer(learning_rate=learning_rate_d).minimize(d_loss, var_list=d_vars,
-                                                                                  global_step=global_step_d)
-    trainer_g = tf.train.RMSPropOptimizer(learning_rate=learning_rate_g).minimize(g_loss, var_list=g_vars,
-                                                                                  global_step=global_step_g)
-    # clip discriminator weights
-    d_clip = [v.assign(tf.clip_by_value(v, -0.01, 0.01)) for v in d_vars]
+        t_vars = tf.trainable_variables()
+        d_vars = [var for var in t_vars if 'dis' in var.name]
+        g_vars = [var for var in t_vars if 'gen' in var.name]
+        # test
+        # print(d_vars)
+        global_step_g = tf.Variable(0, trainable=False)
+        learning_rate_g = 0.07
+        learning_rate_g = tf.train.exponential_decay(learning_rate_g, global_step_g, 10000, .3, staircase=True)
+        global_step_d = tf.Variable(0, trainable=False)
+        learning_rate_d = tf.train.exponential_decay(learning_rate_g, global_step_g, 10000, .3, staircase=True)
+        trainer_d = tf.train.RMSPropOptimizer(learning_rate=learning_rate_d).minimize(d_loss, var_list=d_vars,
+                                                                                      global_step=global_step_d)
+        trainer_g = tf.train.RMSPropOptimizer(learning_rate=learning_rate_g).minimize(g_loss, var_list=g_vars,
+                                                                                      global_step=global_step_g)
+        # clip discriminator weights
+        d_clip = [v.assign(tf.clip_by_value(v, -0.01, 0.01)) for v in d_vars]
 
-    batch_size = BATCH_SIZE
-    image_batch, samples_num = process_data()
+        batch_size = BATCH_SIZE
+        image_batch, samples_num = process_data()
 
-    batch_num = int(samples_num / batch_size)
-    total_batch = 0
-    sess = tf.Session()
-    saver = tf.train.Saver()
-    sess.run(tf.global_variables_initializer())
-    sess.run(tf.local_variables_initializer())
-    # continue training
-    save_path = saver.save(sess, "/tmp/model.ckpt")
-    ckpt = tf.train.latest_checkpoint('./model/' + version)
-    saver.restore(sess, save_path)
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        batch_num = int(samples_num / batch_size)
+        total_batch = 0
+        sess = tf.Session()
+        saver = tf.train.Saver()
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
+        # continue training
+        save_path = saver.save(sess, "/tmp/model.ckpt")
+        ckpt = tf.train.latest_checkpoint('./model/' + version)
+        saver.restore(sess, save_path)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
-    print('total training sample num:%d' % samples_num)
-    print('batch size: %d, batch num per epoch: %d, epoch num: %d' % (batch_size, batch_num, EPOCH))
-    print('start training...')
-    for i in range(EPOCH):
-        print('i: ',i)
-        for j in range(batch_num):
-            print('j: ',j)
-            d_iters = 5
-            g_iters = 1
+        print('total training sample num:%d' % samples_num)
+        print('batch size: %d, batch num per epoch: %d, epoch num: %d' % (batch_size, batch_num, EPOCH))
+        print('start training...')
+        for i in range(EPOCH):
+            print('i: ',i)
+            for j in range(batch_num):
+                print('j: ',j)
+                d_iters = 5
+                g_iters = 1
 
-            train_noise = np.random.uniform(-1.0, 1.0, size=[batch_size, random_dim]).astype(np.float32)
-            for k in range(d_iters):
-                with tf.device('/gpu:0'):
-                    train_image = sess.run(image_batch)
-                    # wgan clip weights
-                    sess.run(d_clip)
-                    # Update the discriminator
-                    _, dLoss = sess.run([trainer_d, d_loss],
-                                        feed_dict={random_input: train_noise, real_image: train_image, is_train: True})
-            # Update the generator
-            for k in range(g_iters):
-                with tf.device('/gpu:1'):
-                    # train_noise = np.random.uniform(-1.0, 1.0, size=[batch_size, random_dim]).astype(np.float32)
-                    _, gLoss = sess.run([trainer_g, g_loss],
-                                        feed_dict={random_input: train_noise, is_train: True})
+                train_noise = np.random.uniform(-1.0, 1.0, size=[batch_size, random_dim]).astype(np.float32)
+                for k in range(d_iters):
+                    with tf.device('/gpu:0'):
+                        train_image = sess.run(image_batch)
+                        # wgan clip weights
+                        sess.run(d_clip)
+                        # Update the discriminator
+                        _, dLoss = sess.run([trainer_d, d_loss],
+                                            feed_dict={random_input: train_noise, real_image: train_image, is_train: True})
+                # Update the generator
+                for k in range(g_iters):
+                    with tf.device('/gpu:1'):
+                        # train_noise = np.random.uniform(-1.0, 1.0, size=[batch_size, random_dim]).astype(np.float32)
+                        _, gLoss = sess.run([trainer_g, g_loss],
+                                            feed_dict={random_input: train_noise, is_train: True})
 
-        # save check point every 500 epoch
-        if i % 500 == 0:
-            if not os.path.exists('./model/' + version):
-                os.makedirs('./model/' + version)
-            saver.save(sess, './model/' + version + '/' + str(i))
-        if i % 50 == 0:
-            # save images
-            if not os.path.exists(new_covers_path):
-                os.makedirs(new_covers_path)
-            sample_noise = np.random.uniform(-1.0, 1.0, size=[batch_size, random_dim]).astype(np.float32)
-            imgtest = sess.run(fake_image, feed_dict={random_input: sample_noise, is_train: False})
-            save_images(imgtest, [8, 8], new_covers_path + '/epoch' + str(i) + '.jpg')
+            # save check point every 500 epoch
+            if i % 500 == 0:
+                if not os.path.exists('./model/' + version):
+                    os.makedirs('./model/' + version)
+                saver.save(sess, './model/' + version + '/' + str(i))
+            if i % 50 == 0:
+                # save images
+                if not os.path.exists(new_covers_path):
+                    os.makedirs(new_covers_path)
+                sample_noise = np.random.uniform(-1.0, 1.0, size=[batch_size, random_dim]).astype(np.float32)
+                imgtest = sess.run(fake_image, feed_dict={random_input: sample_noise, is_train: False})
+                save_images(imgtest, [8, 8], new_covers_path + '/epoch' + str(i) + '.jpg')
 
-            print('train:[%d],d_loss:%f,g_loss:%f' % (i, dLoss, gLoss))
-    coord.request_stop()
-    coord.join(threads)
+                print('train:[%d],d_loss:%f,g_loss:%f' % (i, dLoss, gLoss))
+        coord.request_stop()
+        coord.join(threads)
 
 
 if __name__ == "__main__":
